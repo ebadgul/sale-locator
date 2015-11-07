6093910e6f55db89f0589990b96ba36a7f82f98f
Ebadullah Gul <ebadgul66@gmail.com>
2015-11-07 20:23:32 +0000

    configured added fields with parse.com

--- app/src/main/java/ie/wit/www/salelocator/SignupActivity.java
+++ app/src/main/java/ie/wit/www/salelocator/SignupActivity.java
@@ -18,6 +18,16 @@ public class SignupActivity extends AppCompatActivity {
 
     protected EditText usernameInput;
     protected EditText passwordInput;
+
+    protected EditText passwordInputConf;
+    protected EditText shopNameInput;
+    protected EditText shopAddressInput;
+
+    protected String shopName;
+    protected String shopAddress;
+    protected String passwordConfirm;
+
+
     protected Button signup_btn;
     protected String username;
     protected String password;
@@ -44,6 +54,9 @@ public class SignupActivity extends AppCompatActivity {
 
 
         usernameInput = (EditText) findViewById(R.id.signup_username);
+        shopNameInput = (EditText) findViewById(R.id.signup_shopname);
+        shopAddressInput = (EditText) findViewById(R.id.signup_shopAddress);
+        passwordInputConf = (EditText) findViewById(R.id.signup_passwordC);
         passwordInput = (EditText) findViewById(R.id.signup_password);
         signup_btn = (Button) findViewById(R.id.signupBtn);
 
@@ -51,18 +64,28 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 username = usernameInput.getText().toString();
+                shopName = shopNameInput.getText().toString();
+                shopAddress = shopAddressInput.getText().toString();
                 password = passwordInput.getText().toString();
+                passwordConfirm = passwordInputConf.getText().toString();
+
+
 
                 ParseUser user = new ParseUser();
                 user.setUsername(username);
+                user.put("shopname", shopName);
+                user.put("shopaddress", shopAddress);
                 user.setPassword(password);
+                user.put("passwordconfirmation", passwordConfirm);
+
+//                user.setShopname()
 
                 user.signUpInBackground(new SignUpCallback() {
                     @Override
                     public void done(ParseException e) {
-                        if (e == null){
+                        if (e == null) {
                             Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-                        }else {
+                        } else {
                             Toast.makeText(getApplicationContext(), "an error occured", Toast.LENGTH_LONG).show();
                         }
                     }
